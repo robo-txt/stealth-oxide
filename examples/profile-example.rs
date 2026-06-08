609e559ffab3dca1ad8337b659b0549fc466b102
Robot_txt <robot.txt17@gmail.com>
2026-06-07 22:39:28 -0400

    added example for profiles

---
+++ examples/profile-example.rs
@@ -0,0 +1,21 @@
+use anyhow::Result;
+
+use stealth_oxide::browser::StealthBrowser;
+use stealth_oxide::profiles::chrome_windows::chrome_windows;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let profile = chrome_windows();
+
+    let browser = StealthBrowser::launch(profile).await?;
+
+    let page = browser.new_page("https://example.com").await?;
+
+    let html = page.inner().content().await?;
+
+    println!("{html}");
+
+    browser.close().await?;
+
+    Ok(())
+}
